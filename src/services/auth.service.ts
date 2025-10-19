2713b8e29556832b1f7ee98c60b1295f87ea4a3d
unknown <ranarr.dev@gmail.com>
2025-10-19 22:20:53 +0600

    feat: login faeture added

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,3 +5,8 @@ export const registerUser = async (payload: any) => {
     const { data } = await api.post("/auth/register", payload);
     return data;
 };
+
+export const loginUser = async (payload: { email: string; password: string }) => {
+    const { data } = await api.post("/auth/login", payload);
+    return data;
+};
